7c465a17013cf9a5414f0ddfbc18a019c2dd8da6
ss9424 <shreshthsharma1411@gmail.com>
2025-03-28 02:13:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -52,4 +52,4 @@ def analyze(analysis_type):
     return jsonify({analysis_type: results.get(analysis_type, "Invalid analysis type")})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0" , port=5000)
